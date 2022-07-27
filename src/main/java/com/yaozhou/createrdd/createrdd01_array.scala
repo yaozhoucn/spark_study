f2cedec5a6a3c18d24c29f8e30efe7aef7f3c95e
hang <wangxiao_h@126.com>
2022-07-27 09:57:31 +0800

    Initial commit

---
+++ src/main/java/com/yaozhou/createrdd/createrdd01_array.scala
@@ -0,0 +1,12 @@
+package com.yaozhou.createrdd
+
+/**
+ * @Author: HANG
+ * @Date: 2022/7/27 9:56
+ * @Desc:
+ */
+object createrdd01_array {
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
